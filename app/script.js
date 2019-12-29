b1591f5cadfdaa8490a307db290581a46de1edda
YurokBo <vertel3012@mail.ru>
2019-12-29 19:57:26 +0300

    script

--- app/script.js
+++ app/script.js
@@ -1,5 +1,29 @@
 const btn = document.querySelector('.btn');
+const myForm = document.getElementById('my-form');
 
-btn.addEventListener('click', e => {
+
+myForm.addEventListener('submit', async e => {
     e.preventDefault();
+
+    const formData = new FormData(e.target),
+
+        response = await fetch ('https://netology-slow-rest.herokuapp.com/auth.php', {
+            method: 'post',
+            body: formData
+        });
+
+    const json = await response.json();
+
+    if(json.success) {
+        // signActive.classList.remove('signin_active');
+
+        const welcome = document.querySelector('.welcome'),
+            userId = welcome.querySelector('#user_id');
+
+        welcome.classList.add('welcome_active');
+        userId.innerHTML = json.user_id;
+    }
+
+
 })
+
